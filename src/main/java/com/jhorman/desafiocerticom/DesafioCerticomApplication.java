5ced197a5736665fd89ea3fba7d29f8ddbaccf13
Jhorman Rus <jhormanrus@gmail.com>
2022-03-20 17:35:43 -0500

    Initial commit

---
+++ src/main/java/com/jhorman/desafiocerticom/DesafioCerticomApplication.java
@@ -0,0 +1,13 @@
+package com.jhorman.desafiocerticom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioCerticomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioCerticomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jhorman/desafiocerticom/DesafioCerticomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhorman.desafiocerticom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioCerticomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
